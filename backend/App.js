6d6e0a8934c22e380b4927de7e4307d49a21a035
shreyshah-06 <shrey.shah0610@gmail.com>
2025-01-06 23:41:34 +0530

    Added Rate Limiter

--- backend/App.js
+++ backend/App.js
@@ -3,8 +3,19 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const db = require("./config/database");
+const rateLimit = require("express-rate-limit");
 
 const PORT = process.env.PORT || 5001;
+
+// Set up Rate Limiting
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 2 minutes
+  max: 100,
+  message: "Too many requests from this IP, please try again later.",
+  headers: true,
+})
+
+app.use(limiter);
 app.use(cors());
 app.use(express.json());
 
--- frontend/src/helper/axios.js
+++ frontend/src/helper/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const token = window.localStorage.getItem("token");
 const API_URI = process.env.REACT_APP_BASE_URL_API
-// const API_URI = 'http://localhost:5000/api'
 
 const axiosInstance = axios.create({
   baseURL: API_URI,
