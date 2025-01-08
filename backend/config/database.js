9aca8026e3a77563f083c2a0e6708f330b7a9cea
shreyshah-06 <shrey.shah0610@gmail.com>
2025-01-09 01:03:04 +0530

    Postgres on AWS RDS

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,28 @@
 const Sequelize = require('sequelize')
-module.exports = new Sequelize('ezfinance','postgres','admin',{
-    host: 'localhost',
-    dialect:"postgres",
-});
+// module.exports = new Sequelize('ezfinance','postgres','admin',{
+//     host: 'localhost',
+//     dialect:"postgres",
+// });
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+    console.error('DATABASE_URL environment variable is not set');
+    process.exit(1);  // Stop execution if the database URL is not provided
+}
+
+module.exports = new Sequelize(DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false  // SSL support for AWS RDS
+      }
+    },
+    logging: false,  // Disable logging if not needed
+    pool: {
+      max: 5, // Max number of connections
+      min: 0, // Min number of connections
+      acquire: 30000, // Max time (in ms) to wait for a connection
+      idle: 10000 // Max time (in ms) a connection can be idle before being released
+    }
+  });
